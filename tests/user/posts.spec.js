ea6ab57ab7f577a0b9cdb28c491dffc2795cc131
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-21 13:45:02 +0200

    Added test for deleting a post

--- tests/user/posts.spec.js
+++ tests/user/posts.spec.js
@@ -7,6 +7,8 @@ import {
 } from "../url";
 import { STATUS_CODE_SUCCESS } from "../statuscode";
 
+// TODO : CLEAN UP THIS CODE !
+
 test.beforeEach(async ({ page }) => {
   await page.goto(LOGIN_CLIENT);
 
@@ -69,4 +71,70 @@ test("Should create a new post", async ({ page }) => {
   }
 
   expect(isPostCreated).toBe(true);
+});
+
+test("Should delete a post", async ({ page }) => {
+  const createdPostTitle = `Post for Deletion - ${Date.now()}`;
+  const postText = `This post will be deleted - ${Math.random()
+    .toString(36)
+    .substring(7)}`;
+
+  await expect(page).toHaveURL(USER_INFO_CLIENT);
+  await page.click('button:text("Create Post")');
+
+  await page.waitForSelector('input[placeholder="Title"]', {
+    state: "visible",
+  });
+  await page.waitForSelector('textarea[placeholder="Post Text"]', {
+    state: "visible",
+  });
+
+  await page.fill('input[placeholder="Title"]', createdPostTitle);
+  await page.fill('textarea[placeholder="Post Text"]', postText);
+
+  await page.locator('input[placeholder="Title"]').evaluate((el) => el.blur());
+
+  const createButton = page.locator('.chakra-portal button:text("Create")');
+  await createButton.click();
+
+  try {
+    await page.waitForResponse(
+      (response) =>
+        response.url() === USER_POSTS_SERVER &&
+        response.status() === STATUS_CODE_SUCCESS
+    );
+  } catch (error) {
+    console.error("200 response for creating a post was not received:", error);
+  }
+
+  let isPostDeleted = false;
+
+  page.on("response", (response) => {
+    if (
+      response.url() === USER_POSTS_SERVER &&
+      response.status() === STATUS_CODE_SUCCESS
+    ) {
+      isPostDeleted = true;
+    }
+  });
+
+  const postsContainer = page.locator("div.css-189aeu6");
+  const specificPostDivLocator = postsContainer.locator(
+    `div.css-143t1la:has-text("${createdPostTitle}")`
+  );
+
+  await expect(specificPostDivLocator).toBeVisible();
+  const postIdToDelete = await specificPostDivLocator.getAttribute("id");
+
+  if (postIdToDelete) {
+    const deleteButtonLocator = specificPostDivLocator.locator(
+      'button:has-text("Delete")'
+    );
+    await expect(deleteButtonLocator).toBeEnabled();
+    await deleteButtonLocator.click();
+
+    await expect(specificPostDivLocator).not.toBeVisible();
+  } else {
+    console.error("Error: Could not retrieve ID for the specific post div.");
+  }
 });
