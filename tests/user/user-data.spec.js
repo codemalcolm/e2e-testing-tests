a32af05a0c0adaa78db516b372590e86d6ea4e09
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-21 10:34:47 +0200

    added test for not fetching user posts

---
+++ tests/user/user-data.spec.js
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+import {
+  STATUS_CODE_FORBIDDEN,
+  USER_INFO_CLIENT,
+  USER_POSTS_SERVER,
+} from "../variables.js";
+
+test("Shouldn't fetch user posts", async ({ page }) => {
+  let isNotFetched = false;
+
+  page.on("response", (response) => {
+    if (
+      response.url() === USER_POSTS_SERVER &&
+      response.status() === STATUS_CODE_FORBIDDEN
+    ) {
+      isNotFetched = true;
+    }
+  });
+
+  await page.goto(USER_INFO_CLIENT);
+
+  await page
+    .waitForResponse(
+      (response) =>
+        response.url() === USER_POSTS_SERVER &&
+        response.status() === STATUS_CODE_FORBIDDEN
+    )
+    .catch((error) => {
+      console.error(
+        "403 response for fetching user info was not received:",
+        error
+      );
+    });
+
+  expect(isNotFetched).toBe(true);
+});
