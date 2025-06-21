686ad2488b852ab31036e4f900e3cae3c6961d18
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-21 10:34:29 +0200

    Added variables for posts and user info

--- tests/variables.js
+++ tests/variables.js
@@ -1,13 +1,14 @@
-import { test, expect } from "@playwright/test";
-
 export const DASHBOARD_SERVER = "http://localhost:5000/dashboard";
 export const DASHBOARD_CLIENT = "http://localhost:3000/dashboard";
 export const REGISTER_SERVER = "http://localhost:5000/register";
 export const REGISTER_CLIENT = "http://localhost:3000/register";
 export const LOGIN_SERVER = "http://localhost:5000/login";
 export const LOGIN_CLIENT = "http://localhost:3000/login";
+export const USER_INFO_SERVER = "http://localhost:5000/user-info";
+export const USER_INFO_CLIENT = "http://localhost:3000/user-page";
+export const USER_POSTS_SERVER = "http://localhost:5000/user-info/posts";
 
 export const STATUS_CODE_FORBIDDEN = 403;
-export const STATUS_CODE_BAD_REQUEST = 400;
 export const STATUS_CODE_CREATED = 201;
 export const STATUS_CODE_SUCCESS = 200;
+export const STATUS_CODE_BAD_REQUEST = 400;
