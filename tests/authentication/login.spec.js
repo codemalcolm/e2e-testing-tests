881e10ffddeb35e577c3bdc53f0c0a3e0d714106
Danosch <dennis.flegler@student.fhws.de>
2025-06-22 12:38:01 +0200

    adjusted tests wip in login

--- tests/authentication/login.spec.js
+++ tests/authentication/login.spec.js
@@ -2,83 +2,52 @@ import { test, expect } from "@playwright/test";
 import {
   LOGIN_CLIENT,
   LOGIN_SERVER,
-  REGISTER_CLIENT,
   USER_INFO_CLIENT,
   USER_INFO_SERVER,
-} from "../url";
-import { STATUS_CODE_SUCCESS, STATUS_CODE_BAD_REQUEST } from "../statuscode";
-import { USER, UNKNOWN_USER } from "../user-data";
+  USER,
+  UNKNOWN_USER,
+  STATUS_CODE_SUCCESS,
+  STATUS_CODE_BAD_REQUEST,
+  register,
+  ALL_DATA
+} from "../utility";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(REGISTER_CLIENT);
-
-  await page.fill('input[placeholder="Username"]', USER.username);
-  await page.fill('input[placeholder="Password"]', USER.password);
-
-  await page.click('button:text("Register")');
+  await register(page, USER);
 });
 
 test("Should allow to login User", async ({ page }) => {
-  let isLoggedIn = false;
-
-  page.on("response", (response) => {
-    if (
-      response.url() === LOGIN_SERVER &&
-      response.status() === STATUS_CODE_SUCCESS
-    ) {
-      isLoggedIn = true;
-    }
-  });
-
   await page.goto(LOGIN_CLIENT);
-
   await page.fill('input[placeholder="Username"]', USER.username);
   await page.fill('input[placeholder="Password"]', USER.password);
 
-  await page.click('button:text("Login")');
+  const [response] = await Promise.all([
+    page.waitForResponse(
+      (res) =>
+        res.url() === LOGIN_SERVER && res.status() === STATUS_CODE_SUCCESS
+    ),
+    page.click('button:text("Login")'),
+  ]);
 
-  try {
-    await page.waitForResponse(
-      (response) =>
-        response.url() === LOGIN_SERVER &&
-        response.status() === STATUS_CODE_SUCCESS
-    );
-  } catch (error) {
-    console.error("200 response for login was not received:", error);
-  }
+  expect(response.status()).toBe(STATUS_CODE_SUCCESS);
 
-  expect(isLoggedIn).toBe(true);
 });
 
 test("Should deny access without an unregistered user", async ({ page }) => {
-  let isLoggedIn = false;
-
-  page.on("response", (response) => {
-    if (
-      response.url() === LOGIN_SERVER &&
-      response.status() === STATUS_CODE_BAD_REQUEST
-    ) {
-      isLoggedIn = true;
-    }
-  });
-
   await page.goto(LOGIN_CLIENT);
   await page.fill('input[placeholder="Username"]', UNKNOWN_USER.username);
   await page.fill('input[placeholder="Password"]', UNKNOWN_USER.password);
 
-  await page.click('button:text("Login")');
+  const [response] = await Promise.all([
+    page.waitForResponse(
+      (res) => 
+        res.url() === LOGIN_SERVER && res.status() === STATUS_CODE_BAD_REQUEST
+    ),
+     page.click('button:text("Login")'),
+  ]);
 
-  try {
-    await page.waitForResponse(
-      (response) =>
-        response.url() === LOGIN_SERVER &&
-        response.status() === STATUS_CODE_BAD_REQUEST
-    );
-  } catch (error) {
-    console.error("400 response for login was not received:", error);
-  }
+  expect(response.status()).toBe(STATUS_CODE_BAD_REQUEST)
 
-  expect(isLoggedIn).toBe(true);
 });
 
 test("should return user info", async ({ page }) => {
@@ -107,3 +76,12 @@ test("should return user info", async ({ page }) => {
 
   await expect(page.locator("text=Username: " + USER.username)).toBeVisible();
 });
+
+test.afterEach(async ({ request }) => {
+
+  const response = await request.delete(`${ALL_DATA}`);
+
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(STATUS_CODE_SUCCESS);
+
+})
--- tests/utility.js
+++ tests/utility.js
@@ -20,12 +20,23 @@ export const STATUS_CODE_CREATED = 201;
 export const STATUS_CODE_SUCCESS = 200;
 export const STATUS_CODE_BAD_REQUEST = 400;
 
-export async function register(request, user = USER) {
-    const response = await request.post(REGISTER_SERVER, {
-        data: { username: user.username, password: user.password }
-    });
-    expect(response.status()).toBe(STATUS_CODE_CREATED);
-    return response.json();
+export async function register(page, user = USER) {
+    await page.goto(REGISTER_CLIENT);
+  await page.fill('input[placeholder="Username"]', user.username);
+  await page.fill('input[placeholder="Password"]', user.password);
+
+  const [response] = await Promise.all([
+    page.waitForResponse(
+      (res) => res.url() === REGISTER_SERVER && res.status() === STATUS_CODE_CREATED
+    ),
+    page.click('button:text("Register")'),
+  ]);
+
+  expect(response.status()).toBe(STATUS_CODE_CREATED);
+  const body = await response.json();
+  expect(body.message).toBe("User created");
+
+  return response;
 }
 
 export async function login(request, user = USER) {
