232969a279c56b3dc6e8a61777d4b2bb4aed59d8
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-22 10:43:34 +0200

    Fixed bug of racing conditions in a  flaky "Should allow to register User" test & added assert for response message

--- tests/authentication/register.spec.js
+++ tests/authentication/register.spec.js
@@ -1,80 +1,41 @@
-import {
-  test,
-  expect
-} from "@playwright/test";
-import {
-  REGISTER_CLIENT,
-  REGISTER_SERVER
-} from "../url";
-import {
-STATUS_CODE_CREATED,
-STATUS_CODE_BAD_REQUEST
-} from "../statuscode";
-import{
-  USER
-} from "../user-data"
+import { test, expect } from "@playwright/test";
+import { REGISTER_CLIENT, REGISTER_SERVER } from "../url";
+import { STATUS_CODE_CREATED, STATUS_CODE_BAD_REQUEST } from "../statuscode";
+import { USER } from "../user-data";
 
 test("Should allow to register User", async ({ page }) => {
-  let isRegistered = false;
-
-  page.on("response", (response) => {
-    if (
-      response.url() === REGISTER_SERVER &&
-      response.status() === STATUS_CODE_CREATED
-    ) {
-      isRegistered = true;
-    }
-  });
-
   await page.goto(REGISTER_CLIENT);
 
   await page.fill('input[placeholder="Username"]', USER.username);
   await page.fill('input[placeholder="Password"]', USER.password);
 
-  await page.click('button:text("Register")');
+  const [response] = await Promise.all([
+    page.waitForResponse(
+      (res) =>
+        res.url() === REGISTER_SERVER && res.status() === STATUS_CODE_CREATED
+    ),
+    page.click('button:text("Register")'),
+  ]);
 
-  try {
-    await page.waitForResponse(
-      (response) =>
-        response.url() === REGISTER_SERVER &&
-        response.status() === STATUS_CODE_CREATED
-    );
-  } catch (error) {
-    console.error("201 response for registration was not received:", error);
-  }
-
-  expect(isRegistered).toBe(true);
+  expect(response.status()).toBe(STATUS_CODE_CREATED);
+  const responseBody = await response.json();
+  expect(responseBody.message).toBe("User created");
 });
 
-
 test("Should not allow to register User", async ({ page }) => {
-  let isRegistered = false;
-
-  page.on("response", (response) => {
-    if (
-      response.url() === REGISTER_SERVER &&
-      response.status() === STATUS_CODE_CREATED
-    ) {
-      isRegistered = true;
-    }
-  });
-
   await page.goto(REGISTER_CLIENT);
 
   await page.fill('input[placeholder="Username"]', "");
   await page.fill('input[placeholder="Password"]', "");
 
-  await page.click('button:text("Register")');
+  const [response] = await Promise.all([
+    page.waitForResponse(
+      (res) =>
+        res.url() === REGISTER_SERVER &&
+        res.status() === STATUS_CODE_BAD_REQUEST
+    ),
+    page.click('button:text("Register")'),
+  ]);
 
-  try {
-    await page.waitForResponse(
-      (response) =>
-        response.url() === REGISTER_SERVER &&
-        response.status() === STATUS_CODE_BAD_REQUEST
-    );
-  } catch (error) {
-    console.error("201 response for registration was not received:", error);
-  }
-
-  expect(isRegistered).toBe(false);
+  expect(response.status()).toBe(STATUS_CODE_BAD_REQUEST);
 });
